01596d72ec25bb86f73ab9c83555d5ea6996793d
roi <roi@ubuntu>
2017-08-02 11:21:36 +0800

    Added DateTimeUtil

--- src/test/java/com/team2/forex/DateTimeUtilTest.java
+++ src/test/java/com/team2/forex/DateTimeUtilTest.java
@@ -1,6 +1,10 @@
 package com.team2.forex;
 
+import static org.junit.Assert.assertEquals;
+
 import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.Test;
@@ -16,9 +20,25 @@ public class DateTimeUtilTest {
 	
 	@Test
 	public void timeStampToString(){
-		//Date date = new Date();
-		//String result = DateTimeUtil.timestampToString(new Timestamp(date.getTime()));
- 		
+		Date date = new Date();
+		SimpleDateFormat sdf= new SimpleDateFormat ("yyyy-MM-dd_HH:mm:ss.SSS");
+		
+		String result = DateTimeUtil.timestampToString(new Timestamp(date.getTime()));
+ 		assertEquals("expected string should be ", sdf.format(date),result);	
 	}
 	
+	@Test
+	public void stringToTimeStamp() throws ParseException{
+		Date date = new Date();
+		SimpleDateFormat sdf= new SimpleDateFormat ("yyyy-MM-dd_HH:mm:ss.SSS");
+		String stringTs = "2016-06-23_22:04:53.078";
+		
+		
+		Timestamp ts = DateTimeUtil.stringToTimestamp(stringTs);
+		
+		Timestamp expectedTimestamp = new Timestamp(sdf.parse(stringTs).getTime());	
+		assertEquals("expected timestamp is wrong",expectedTimestamp, ts);
+		
+		
+	}
 }
