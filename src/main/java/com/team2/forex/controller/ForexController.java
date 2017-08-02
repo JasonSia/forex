ddfb774e59a8cf7657f16b58b171b3232c52f86a
roi <roi@ubuntu>
2017-08-01 23:42:35 -0700

    Story 1 - updates user auth

--- src/main/java/com/team2/forex/controller/ForexController.java
+++ src/main/java/com/team2/forex/controller/ForexController.java
@@ -6,10 +6,14 @@ import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
+import javax.servlet.http.HttpSession;
+
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,6 +41,8 @@ public class ForexController {
 	// produces={MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_XML_VALUE}
 	@RequestMapping(value="/placeMarketOrder", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
 	public String createMarketOrder(@RequestBody Order userOrder){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String userid = auth.getName();
 		int flagBuy=0;
 		int flagSell=0;
 		for(Currency c: Currency.values()){
@@ -46,13 +52,13 @@ public class ForexController {
 				flagSell=1;
 		}
 		if(!(userOrder.getOrderType().equals("BUY") || userOrder.getOrderType().equals("SELL"))){
-			return "OrderType should only be BUY or SELL";
+			return "ERROR: OrderType should only be BUY or SELL";
 		}
 		else if(flagBuy!=1)
-		{   return "Buy Currency not supported.";	
+		{   return "ERROR: Buy Currency not supported.";	
 		}
 		else if(flagSell!=1)
-		{   return "Sell Currency not supported.";	
+		{   return "ERROR: Sell Currency not supported.";	
 		}
 		else
 		{
@@ -60,7 +66,8 @@ public class ForexController {
 		userOrder.setSubmittedTime(new Timestamp(System.currentTimeMillis()));
 		userOrder.setCurrencyBuy(Currency.valueOf(userOrder.getCurrencyBuyInput()));
 		userOrder.setCurrencySell(Currency.valueOf(userOrder.getCurrencySellInput()));
-		return "Your order's unique ID is: "+mos.placeMarketOrder(userOrder);
+		userOrder.setUserId(userid);
+		return "SUCCESSFUL: Your order is placed and the unique ID is: "+mos.placeMarketOrder(userOrder);
 		}
 	}
 	
