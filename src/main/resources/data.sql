081ac0ff57843eba5f1f4b58879fa8f7eda58c46
roi <roi@ubuntu>
2017-08-03 09:59:39 +0800

    Refactor ordernumber longer than expected

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,22 +8,20 @@ insert into users(userid,password,role) values ('client4','client4','user');
 
 insert into orderList(orderType, currencyBuy, currencySell, 
 size, preferredPrice,status, goodTillDate, submittedTime,userid,orderNumber)
-values ('limit', 'USD', 'CAD', 100, 2.00, 'NOTFILLED', '2020-07-30 23:04:13', '2017-08-01 23:04:15','client','c81b59cb37dbe7d2607432877c12cac29ce53c39
-
-');
+values ('limit', 'USD', 'CAD', 100, 2.00, 'NOTFILLED', '2020-07-30 23:04:13', '2017-08-01 23:04:15','client','c81b59cb37dbe7d2607432877c12cac2');
 
 insert into orderList(orderType, currencyBuy, currencySell, 
 size, preferredPrice,status, goodTillDate, submittedTime,userid,orderNumber )
-values ('limit', 'CAD', 'USD', 200, 5.00, 'NOTFILLED', '2020-07-30 23:04:13', '2017-08-01 23:04:16','client2','1055bebcdcd7de41df33cdaf8ab31d02dea1e214');
+values ('limit', 'CAD', 'USD', 200, 5.00, 'NOTFILLED', '2020-07-30 23:04:13', '2017-08-01 23:04:16','client2','c81b59cb37dbe7d2607432877c234ac2');
 
 insert into orderList(orderType, currencyBuy, currencySell, 
 size, preferredPrice,status, goodTillDate, submittedTime,userid,orderNumber )
-values ('limit', 'SGD', 'AUD', 100, 2.00, 'NOTFILLED', '2020-07-30 23:04:13', '2017-08-01 23:09:13','client3','d70cb6979e51f46d026ae6961be41252d0644cf4');
+values ('limit', 'SGD', 'AUD', 100, 2.00, 'NOTFILLED', '2020-07-30 23:04:13', '2017-08-01 23:09:13','client3','c81b587640dbe7d2607432877c234ac2');
 
 insert into orderList(orderType, currencyBuy, currencySell, 
 size, preferredPrice,status, goodTillDate, submittedTime,userid,orderNumber )
-values ('limit', 'AUD', 'SGD', 100, 2.00, 'NOTFILLED', '2020-07-30 23:04:13', '2017-08-01 23:10:13','client4','f828702beda27ab61e83867d8ffdf183d22e10ae');
+values ('limit', 'AUD', 'SGD', 100, 2.00, 'NOTFILLED', '2020-07-30 23:04:13', '2017-08-01 23:10:13','client4','c81b59cb37dbe7d2607432877c289564');
 
 insert into orderList(orderType, currencyBuy, currencySell, 
 size, preferredPrice,status, goodTillDate, submittedTime,userid,orderNumber )
-values ('limit', 'AUD', 'SGD', 100, 2.00, 'FILLED', '2020-07-30 23:04:13', '2017-08-01 23:10:13','client1','ecc1e8561be5da89c604305807c3ab46f5c1e48f');
+values ('limit', 'AUD', 'SGD', 100, 2.00, 'FILLED', '2020-07-30 23:04:13', '2017-08-01 23:10:13','client1','c81idosp37dbe7d2607432877c234ac2');
